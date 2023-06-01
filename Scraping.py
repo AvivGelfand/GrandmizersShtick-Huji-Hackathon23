4316dd24dbb37383f17573d6b0e3c17066868e50
AvivGelfand <63909805+AvivGelfand@users.noreply.github.com>
2023-06-01 15:53:34 +0300

    PromptEngineered

--- Scraping.py
+++ Scraping.py
@@ -13,7 +13,7 @@ def create_soup(url):
     :return: The BeautifulSoup object.
     """
     response = requests.get(url)  # Make the HTML request
-    soup = BeautifulSoup(response.content, 'lxml')  # Get the content of the website
+    soup = BeautifulSoup(response.content, "lxml")  # Get the content of the website
     return soup
 
 
@@ -26,7 +26,7 @@ def extract_article_content(url):
     """
     soup = create_soup(url)  # Get the page content of the article
     article = ""
-    for news in soup.findAll('div', {'class': 'article-body__content'}):
+    for news in soup.findAll("div", {"class": "article-body__content"}):
         article += news.text.strip()  # Extract the article into a string
     return article
 
@@ -37,24 +37,30 @@ def get_today_articles():
 
     :return: A list of article contents.
     """
-    nbc_us_url = 'https://www.nbcnews.com/us-news/'
-    nbc_url = 'https://www.nbcnews.com/'  # Website URL
+    nbc_us_url = "https://www.nbcnews.com/us-news/"
+    nbc_url = "https://www.nbcnews.com/"  # Website URL
     soup = create_soup(nbc_url)  # Get the content of the website
 
     # Create a list of article links containing articles from today on NBC
     links = []
-    for news in soup.findAll('h3',
-                             {'class': re.compile(r'styles_headline__5qvTg f5(.*)')}):  # Choose story headlines only
-        links.append(news.a['href'])  # Append link of article to list of links
+    for news in soup.findAll(
+        "h3", {"class": re.compile(r"styles_headline__5qvTg f5(.*)")}
+    ):  # Choose story headlines only
+        links.append(news.a["href"])  # Append link of article to list of links
         break
 
     # Create a list of article contents
     article_contents = []  # A list that contains all the contents
-    for link in links:  # Iterate through the links of the different articles to extract their contents separately
+    for (
+        link
+    ) in (
+        links
+    ):  # Iterate through the links of the different articles to extract their contents separately
         article = extract_article_content(link)
         article_contents.append(article)  # Append the article to the articles list
         break
     return article_contents
+
 
 # Example usage
 # output = get_today_articles()
--- main.py
+++ main.py
@@ -6,7 +6,17 @@ from text2text import generate_answer
 article_content = get_today_articles()
 
 # Organize the question and add the 'fine tuning' in terms of the question itself
-prompt = "..."
+prompt = f"""Task: Write a general conversation between two citizens to be converted to an audio file, about the text of the article that follows the string TEXT_START and ends in TEXT_STOP. 
+Style: Casual
+Tone: Serious
+Audience: 70-year old
+Length: 3 paragraphs
+Format: text
+
+TEXT_START
+{article_content[0]}
+TEXT_STOP"""
+
 
 # Ask the OpenAI ChatGPT
 # generative_ans = generate_answer(prompt)
