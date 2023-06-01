b85bb5d79a076c6dc30893f059a4397c5518d46a
orineo1 <orineo1@gmail.com>
2023-06-01 15:36:32 +0300

    Cleaning and adding documentation
    Rearrange the sequence of the functions, modify the names, and provide explanations

--- Scraping.py
+++ Scraping.py
@@ -4,39 +4,58 @@ import requests
 import lxml
 import re
 
-def create_soup(link):
-    r = requests.get(link) # make the HTML request
-    soup = BeautifulSoup(r.content,'lxml') # Get the content of the website
+
+def create_soup(url):
+    """
+    Creates a BeautifulSoup object from the provided URL.
+
+    :param url: The URL of the website.
+    :return: The BeautifulSoup object.
+    """
+    response = requests.get(url)  # Make the HTML request
+    soup = BeautifulSoup(response.content, 'lxml')  # Get the content of the website
     return soup
 
-def return_list_of_articles():
 
-    nbc_us_url = 'https://www.nbcnews.com/us-news/'
-    nbc_url = 'https://www.nbcnews.com/' # Websit URL
-    soup = create_soup(nbc_url) # Get the content of the website
+def extract_article_content(url):
+    """
+    Extracts the content of an article from the provided URL.
 
-    # Create a list of article links containing articles from today on NBC
-    links = []
-    for news in soup.findAll('h3',{'class':re.compile(r'styles_headline__5qvTg f5(.*)')}): # Choose story headlines only
-        links.append(news.a['href']) # append link of article to list of links
-
-    # Create a list of article contents
-    article_contents = [] # a list that contains all the contents
-    for link in links: # iterate through the links of the different articles to extract their contents seperately
-        article = extract_article_content(link)
-        article_contents.append(article) # append the article to the articles string
-    return article_contents
-
-def extract_article_content(link):
-    soup = create_soup(link)  # Get the page content of the article
+    :param url: The URL of the article.
+    :return: The extracted article content as a string.
+    """
+    soup = create_soup(url)  # Get the page content of the article
     article = ""
     for news in soup.findAll('div', {'class': 'article-body__content'}):
-        article += news.text.strip()  # extract the article into a string
+        article += news.text.strip()  # Extract the article into a string
     return article
 
 
+def get_today_articles():
+    """
+    Retrieves a list of articles published today on a specific website.
 
+    :return: A list of article contents.
+    """
+    nbc_us_url = 'https://www.nbcnews.com/us-news/'
+    nbc_url = 'https://www.nbcnews.com/'  # Website URL
+    soup = create_soup(nbc_url)  # Get the content of the website
 
-articles = return_list_of_articles()
-print(articles[0])
-print(articles[1])
+    # Create a list of article links containing articles from today on NBC
+    links = []
+    for news in soup.findAll('h3',
+                             {'class': re.compile(r'styles_headline__5qvTg f5(.*)')}):  # Choose story headlines only
+        links.append(news.a['href'])  # Append link of article to list of links
+        break
+
+    # Create a list of article contents
+    article_contents = []  # A list that contains all the contents
+    for link in links:  # Iterate through the links of the different articles to extract their contents separately
+        article = extract_article_content(link)
+        article_contents.append(article)  # Append the article to the articles list
+        break
+    return article_contents
+
+# Example usage
+# output = get_today_articles()
+# print(output[0])
